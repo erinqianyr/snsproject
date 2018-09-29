29de12f65304411c2897c1dd715138b37658b28c
yyyan116 <35853687+yyyan116@users.noreply.github.com>
2018-09-29 15:03:11 -0400

    fc

---
+++ SNSProjectApp/SNSProjectApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SNSProjectApp
+//
+//  Created by Yanran Qian on 9/29/18.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
