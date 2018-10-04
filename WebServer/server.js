0081b77d2b427b1b310fc66f22ded85b6825d597
Lucas Ference <ference.lucas@gmail.com>
2018-10-04 16:03:04 -0400

    Linked with mysql on LucasPc

--- WebServer/server.js
+++ WebServer/server.js
@@ -1,16 +1,29 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql')
 
-const hostname = '127.0.0.1';
+const app = express();
+
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require('./app/routes')(app, {});
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "lucas",
+  password: "admin"
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+con.connect(function(err) {
+  if (err)  {
+    console.log(err);
+  } else {
+    console.log("Connected!");
+  }
 });
 
-// Test
+app.listen(port, () => {
+  console.log('We are live on ' + port);
+});
