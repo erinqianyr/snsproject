07d77c9c047ac3ec962c1c9de4dbf4bd81ffa0a6
Lucas Ference <ference.lucas@gmail.com>
2018-10-11 17:41:10 -0400

    Added basic GET requests.

--- WebServer/app/routes/note_routes.js
+++ WebServer/app/routes/note_routes.js
@@ -1,7 +1,11 @@
 module.exports = function(app, db) {
-  app.post('/notes', (req, res) => {
+  app.post('/', (req, res) => {
     //Create note here
     console.log(req.body)
     res.send('Hello!')
+  });
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
   });
 };
