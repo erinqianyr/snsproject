12d3b95e37a33f9e4453ac0338f3b33cac7dce8a
Lucas Ference <ference.lucas@gmail.com>
2018-11-12 14:07:52 -0500

    Added test database and basic queries.

--- WebServer/app/routes/note_routes.js
+++ WebServer/app/routes/note_routes.js
@@ -1,11 +1,5 @@
 module.exports = function(app, db) {
-  app.post('/', (req, res) => {
-    //Create note here
-    console.log(req.body)
-    res.send('Hello!')
-  });
 
-  app.get('/', (req, res) => {
-    res.send('Hello World!')
-  });
+
+
 };
--- WebServer/server.js
+++ WebServer/server.js
@@ -8,22 +8,57 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require('./app/routes')(app, {});
+// require('./app/routes')(app, {});
 
 var con = mysql.createConnection({
   host: "localhost",
   user: "lucas",
-  password: "admin"
+  password: "admin",
+  database: "demo_sns"
 });
 
 con.connect(function(err) {
-  if (err)  {
-    console.log(err);
-  } else {
-    console.log("Connected!");
-  }
+  if (err) console.log(err);
+  console.log("Connected!")
 });
 
 app.listen(port, () => {
   console.log('We are live on ' + port);
+});
+
+
+
+// Takes a new reader as an input and inserts a new tuple into READERS.
+app.post('/reader_insert', (req, res) => {
+
+  const input = req.body;
+  const reader = {name: input.name, address: input.address,
+                  email: input.email, password: input.password};
+
+  con.query('INSERT INTO READERS SET ?', reader, (err, res) => {
+    if(err) console.log(err);
+  });
+
+  res.send('Request sent successfully!')
+});
+
+// Retrieves the entire READERS table and displays it in browser.
+app.get('/table', (req, res) => {
+
+  con.query('SELECT * FROM READERS', (err, rows) => {
+    if(err) console.log(err);
+
+    // Log the name of each reader in READERS.
+    rows.forEach((row) => {
+      console.log(row.name);
+    });
+
+    res.send(rows);
+  });
+
+});
+
+// Home page for the test server.
+app.get('/', (req, res) => {
+  res.send('Welcome to the test server!')
 });
